5fe0969bf907b9c18f067fe994f242b0fd813e22
khalilaliev <khalilaliev0@gmail.com>
2024-10-30 12:24:11 +0100

    add actions with numbers

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
+
+// Action with numbers
+
 $reminder_of_division = 7 % 3;
 var_dump($reminder_of_division);
+
+echo '<br>';
+
+$int_part_of_division = (int)(7 / 7.15);
+var_dump($int_part_of_division);
+
+echo '<br>';
+
+$sqrt_number = 25;
+var_dump(sqrt($sqrt_number));
+
+echo '<br>';
+
+// Actions with strings
+
+$fourth_word = 'Десять негритят пошли купаться в море';
+
+
 
 ?>
