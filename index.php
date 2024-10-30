07e67247dcd6586c78fb559d040598f7badc145d
khalilaliev <khalilaliev0@gmail.com>
2024-10-30 13:00:34 +0100

    add actions with strings

--- index.php
+++ index.php
@@ -19,7 +19,24 @@ echo '<br>';
 
 // Actions with strings
 
-$fourth_word = 'Десять негритят пошли купаться в море';
+$fourth_word = explode(' ', 'Десять негритят пошли купаться в море');
+var_dump($fourth_word[3]);
+
+echo '<br>';
+
+$get_char = mb_substr('Десять негритят пошли купаться в море', 16, 1);
+var_dump($get_char);
+
+echo '<br>';
+
+$to_upper = 'Десять негритят пошли купаться в море';
+$to_upper = mb_convert_case($to_upper, MB_CASE_TITLE, 'UTF-8');
+var_dump($to_upper);
+
+echo '<br>';
+
+$length_of_string = 'Десять негритят пошли купаться в море';
+var_dump(mb_strlen($length_of_string, 'UTF-8'));
 
 
 
