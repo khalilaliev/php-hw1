ad44a38beb64263c95f264e87131cffb363ace72
khalilaliev <khalilaliev0@gmail.com>
2024-10-31 14:53:42 +0100

    fix to mb_strlen

--- index.php
+++ index.php
@@ -2,80 +2,82 @@
 
 // Action with numbers
 
-$reminder_of_division = 7 % 3;
-var_dump($reminder_of_division);
+ $reminder_of_division = 7 % 3;
+ var_dump($reminder_of_division);
 
-echo '<br>';
+ echo '<br>';
 
-$int_part_of_division = (int)(7 / 7.15);
-var_dump($int_part_of_division);
+ $int_part_of_division = (int)(7 / 7.15);
+ var_dump($int_part_of_division);
 
-echo '<br>';
+ echo '<br>';
 
-$sqrt_number = 25;
-var_dump(sqrt($sqrt_number));
+ $sqrt_number = 25;
+ var_dump(sqrt($sqrt_number));
 
-echo '<br>';
+ echo '<br>';
 
-// Actions with strings
+ // Actions with strings
 
-$fourth_word = explode(' ', 'Десять негритят пошли купаться в море');
-var_dump($fourth_word[3]);
+ $fourth_word = explode(' ', 'Десять негритят пошли купаться в море');
+ var_dump($fourth_word[3]);
 
-echo '<br>';
+ echo '<br>';
 
-$get_char = mb_substr('Десять негритят пошли купаться в море', 16, 1);
-var_dump($get_char);
+ $get_char = mb_substr('Десять негритят пошли купаться в море', 16, 1);
+ var_dump($get_char);
 
-echo '<br>';
+ echo '<br>';
 
-$to_upper = 'Десять негритят пошли купаться в море';
-$to_upper = mb_convert_case($to_upper, MB_CASE_TITLE, 'UTF-8');
-var_dump($to_upper);
+ $to_upper = 'Десять негритят пошли купаться в море';
+ $to_upper = mb_convert_case($to_upper, MB_CASE_TITLE, 'UTF-8');
+ var_dump($to_upper);
 
-echo '<br>';
+ echo '<br>';
 
-$length_of_string = 'Десять негритят пошли купаться в море';
-var_dump(mb_strlen($length_of_string, 'UTF-8'));
+ $length_of_string = 'Десять негритят пошли купаться в море';
+ var_dump(mb_strlen($length_of_string, 'UTF-8'));
 
-echo '<br>';
+ echo '<br>';
 
-// Actions with logic operators
+ // Actions with logic operators
 
-$bool = true;
-$number = 1;
-var_dump($bool == $number);
+ $bool = true;
+ $number = 1;
+ var_dump($bool == $number);
 
-echo '<br>';
+ echo '<br>';
 
-$bool2 = false;
-$number2 = 0;
-var_dump($bool2 === $number2);
+ $bool2 = false;
+ $number2 = 0;
+ var_dump($bool2 === $number2);
 
-echo '<br>';
+ echo '<br>';
 
 $string1 = 'three';
 $string2 = 'три';
+$len1 = mb_strlen($string1);
+$len2 = mb_strlen($string2);
 
-if ($string1 > $string2){
-  echo $string1 . ' is greater than ' . $string2;
-} elseif ($string1 < $string2){
-  echo $string1 . ' is less than ' . $string2;
+if ($len1 > $len2){
+ echo $string1 . ' is greater than ' . $string2;
+} elseif ($len1 < $len2){
+ echo $string1 . ' is less than ' . $string2;
 } else {
-  echo $string1 . ' and ' . $string2 . ' are equal';
+ echo $string1 . ' and ' . $string2 . ' are equal';
 }
 
-echo '<br>';
+ echo '<br>';
 
-$number3 = 125 * 13 + 7;
-$number4 = 223 + 28 * 2;
+ $number3 = 125 * 13 + 7;
+ $number4 = 223 + 28 * 2;
 
-if ($number3 > $number4){
-  var_dump($number3 . ' is greater than ' . $number4);
-} elseif ($number3 < $number4){
-  var_dump($number3 . ' is less than ' . $number4);
-} else {
-  var_dump($number3 . ' and ' . $number4 . ' are equal');
-}
+ if ($number3 > $number4){
+   var_dump($number3 . ' is greater than ' . $number4);
+ } elseif ($number3 < $number4){
+   var_dump($number3 . ' is less than ' . $number4);
+ } else {
+   var_dump($number3 . ' and ' . $number4 . ' are equal');
+ }
 
 ?>
